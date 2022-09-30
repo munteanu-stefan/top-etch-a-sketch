c6814bbfcd9e5c6c1ac26ccd783d206b7a29212c
munteanu-stefan <munteanustefan20132@gmail.com>
2022-09-30 17:15:45 +0300

    Make the canvas drawable

--- js-script.js
+++ js-script.js
@@ -1,15 +1,17 @@
 let gridSize = prompt("Enter desired canvas size (max 100)", 32);
+const canvas = document.querySelector(".canvas");
 
+
+// max of 100 and min of 2
 if (gridSize > 100) {
   gridSize = 100;
 } else if (gridSize < 2) {
   gridSize = 2;
 }
 
-const canvas = document.querySelector(".canvas");
 
 function createGrid(size) {
-  let canvasSize = (gridSize*gridSize);
+  let canvasSize = gridSize * gridSize;
   for (let index = 0; index < canvasSize; index++) {
     const canvasItem = document.createElement("div");
     canvasItem.classList.add("canvas-item");
@@ -18,4 +20,18 @@ function createGrid(size) {
   }
 }
 
+function makeGridItemDrawable(item) {
+  item.classList.add("canvas-item-drawn");
+}
+
+function makeDrawable() {
+  const gridItems = document.querySelectorAll(".canvas-item");
+  gridItems.forEach((item) =>
+    item.addEventListener("mousedown", function () {
+      item.classList.add("canvas-item-drawn");
+    })
+  );
+}
+
 createGrid(gridSize);
+makeDrawable();
